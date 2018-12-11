f793f485f59d9b6f695fc014655aa2c4d28b7355
EMalley <EMalley998@gmail.com>
2018-12-11 10:59:37 -0500

    added buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,64 @@
 $(document).ready(function () {
     //  Global variables
     var topics = ["Jurassic Park", "Space", "Christmas", "Pugs", "Climbing"];
+    var userInput = $("giphySearch").val()
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=jurrasic+park" + "&api_key=BgVoQX2k20CRJluSOkLWaY846Qub3ZzW&limit=10";
 
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=jurrasic+park" + "&api_key=BgVoQX2k20CRJluSOkLWaY846Qub3ZzW&limit=10";
+   
+   
+//    FUNCTIONS
+// ==================================================
+    // Grabs topics array and makes them into buttons. 
+    function displayButtons() {
+        $("#buttonsForGifs").empty();
+        for (j = 0; j < topics.length; j++){
+            var button = $("<button>");
+            button.addClass("gifbutton");         
+            button.attr("data-name", topics[j]);
+            button.text(topics[j]);
+            $("#buttonsForGifs").append(button);
+        }
+    };
+    
 
+
+    
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         //check to see if we see API call
         console.log("success! got data", response)
+        var results = response.data //give access to first level of results from search.
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("rating: " + rating);
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.fixed_height.url);
+            gifDiv.append(p);
+            gifDiv.append(gifImage);
+        }
     });
+    
+
+
+
+
+
+
+    // Call functions and Methonds
+    displayButtons();
+
+
 
     // Pull 10 search items down from GIPHY search
     // add a function to stop giphys on click. (day 3 #15 in RUTSOM)
     // Create an array and loop over it so that buttons generate, and produce giphys
     // Your app should take the topics in this array and create buttons in your HTML.
     //Try using a loop that appends a button for each string in the array.
+    
 
-
-
+    
 })
 
